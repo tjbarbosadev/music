e56e5fa3a06aef1bd1a8733e433d53acd7204e68
Thiago Xanhenha <tjbarbosa.dev@gmail.com>
2023-10-04 20:25:29 -0300

    feat: splitting components

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Roboto', 'ui-sans-serif', 'system-ui'],
+    },
   },
   plugins: [],
 };
